b7bc27a74ceefe7cf90910e5800c38910f75247f
Skyler <sdyessdev@gmail.com>
2013-06-12 15:16:17 -0500

    Some cleanup to the path

---
+++ Launcher/MainWindowForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication3
+{
+    public partial class MainWindowForm : Form
+    {
+        public MainWindowForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //TODO: IMPLEMENT SETTINGS FILE
+            //if first run, load directory box
+            if (!File.Exists("info.txt"))
+            {
+                DirectoryForm firstRun = new DirectoryForm();
+                firstRun.ShowDialog();
+            }
+            string navurl = "battle.net";
+            webBrowser1.Navigate(navurl);
+            webBrowser1.ScrollBarsEnabled = false;
+            StreamReader openFile = new StreamReader("info.txt");
+            this.path = openFile.ReadLine();
+            openFile.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //set realmlist
+            Process.Start(path + "/Data/realmlist.wtf");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //delete cache folder
+            if (Directory.Exists(path + "/Cache"))
+            {
+                Directory.Delete(path, true);
+            }
+            else
+            {
+                //create popup message
+                System.Windows.Forms.MessageBox.Show("Cache file doesn't exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //launch game
+            Process.Start(path + "/Wow.exe");
+            this.Close();
+        }
+
+        private string path;
+    }
+}
