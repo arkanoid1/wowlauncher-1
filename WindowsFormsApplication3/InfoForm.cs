a00bdfbb1bc1f664074474262554bc3f6febcbaa
Skyler <sdyessdev@gmail.com>
2013-06-12 14:41:47 -0500

    Intial commit of launcher, lots to be changed!

---
+++ WindowsFormsApplication3/InfoForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication3
+{
+    public partial class InfoForm : Form
+    {
+        public InfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderBrowserDialog1.SelectedPath;
+                //delete
+                File.Delete("info.txt");
+                StreamWriter saveFile = new StreamWriter("info.txt");
+                saveFile.WriteLine(path);
+                saveFile.Close();
+                this.Close();
+                this.Dispose();
+            }
+        }
+    }
+}
